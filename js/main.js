47c28dd412a18e0d164ac30ee2a151bb83d88cee
Serena <serenafayelombardo@gmail.com>
2020-09-09 10:50:47 +0200

    Gave functionality to the features section to show hide panels

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+//Functionality for the features section to show and hide blocks
+const featureButtons = document.querySelectorAll('.features__button');
+const featureFacts = document.querySelectorAll('.features__fact');
+
+function removeCurrentButton(button) {
+  button.setAttribute('aria-expanded', 'false');
+  button.classList.remove('features__current-button');
+}
+function setNewCurrentButton(button) {
+  button.setAttribute('aria-expanded', 'true');
+  button.classList.add('features__current-button');
+}
+function showHidePanels(buttonId) {
+  featureFacts.forEach((featureFact) => {
+    featureFact.classList.remove('features__current-fact');
+    if (featureFact.id === buttonId) {
+      featureFact.classList.add('features__current-fact');
+    }
+  });
+}
+//Add click to each feature Button to remove current button and add new and show hide panels
+featureButtons.forEach((featureButton) => {
+  featureButton.addEventListener('click', function () {
+    if (featureButton.classList.contains('features__current-button')) {
+      return;
+    }
+    featureButtons.forEach((button) => {
+      removeCurrentButton(button);
+    });
+    setNewCurrentButton(featureButton);
+    const buttonIdFact = featureButton.getAttribute('aria-controls');
+    showHidePanels(buttonIdFact);
+  });
+});
+
+// Functionality for the contact input
 const form = document.querySelector('.contact__form');
 const emailInput = form.querySelector('.contact__input');
 const errorMessage = form.querySelector('.contact__error-message');
